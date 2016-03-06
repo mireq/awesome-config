ed323704986826d6ec76ad5e70d33978cf697c3d
Miroslav Bendík <miroslav.bendik@gmail.com>
2016-03-06 16:49:56 +0100

    Added listener to udisks

---
+++ udisks.lua
@@ -0,0 +1,22 @@
+local capi = { dbus = dbus }
+
+
+function rescan_devices()
+	print("Scanning devices")
+end
+
+
+rescan_devices()
+
+
+if capi.dbus then
+	capi.dbus.add_match("system", "interface='org.freedesktop.DBus.ObjectManager', member='InterfacesAdded'")
+	capi.dbus.add_match("system", "interface='org.freedesktop.DBus.ObjectManager', member='InterfacesRemoved'")
+	capi.dbus.connect_signal("org.freedesktop.DBus.ObjectManager",
+		function (data, text)
+			if data.path == "/org/freedesktop/UDisks2" then
+				rescan_devices()
+			end
+		end
+	);
+end
